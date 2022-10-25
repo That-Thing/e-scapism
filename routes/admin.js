0ba6ac9e4d98d01d960ba6ae89f668f3d43295bf
Simon Novak <sen@onee.ch>
2022-10-25 16:45:39 -0400

    Added forum creation. Added forum listing.

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const config = require('../modules/config');
 const connection = require('../modules/connection');
-
+const { body, validationResult } = require('express-validator');
 
 //Get admin page
 router.get('/', function(req, res) {
@@ -24,6 +24,23 @@ router.get('/forums', function(req, res) {
         res.render('admin-forums', { config: config, group: req.session.group, user: req.session.user, forums: result });
     });
 });
+//Add forum
+router.post('/forums/new', body('name').not().isEmpty().trim().escape(), body("description").not().isEmpty().trim().escape().isLength({max:256}), body("identifier").not().isEmpty().trim().escape().isLength({max:5}), function(req, res) {
+    if(!req.session.loggedIn || req.session.group < 1) {
+        return res.redirect('/');
+    }
+    let name = req.body.name;
+    let description = req.body.description;
+    let identifier = req.body.identifier;
+    if(name == "" || description == "" || identifier == "") { //if any fields are empty
+        return res.redirect('/admin/forums?error='+encodeURIComponent("Please fill out all fields."));
+    }
+    connection.query(`INSERT INTO forums VALUES (NULL, '${identifier}', ${Date.now()}, ${req.session.user}, '${name}', '${description}')`, function (error, result) {
+        if (error) throw error;
+        res.redirect('/admin/forums');
+    });
+});
+
 
 
 
