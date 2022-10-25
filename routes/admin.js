150c632acf8fc6624e2adbd8b83ab066972647f4
Simon Novak <sen@onee.ch>
2022-10-25 17:17:45 -0400

    Added forum editing.

--- routes/admin.js
+++ routes/admin.js
@@ -24,6 +24,24 @@ router.get('/forums', function(req, res) {
         res.render('admin-forums', { config: config, group: req.session.group, user: req.session.user, forums: result });
     });
 });
+//Edit forum
+router.get('/forums/:id', function(req, res) {
+    let error = null
+    if(req.query.error) {
+        error = req.query.error;
+    }
+    if(!req.session.loggedIn || req.session.group < 1) {
+        return res.redirect('/');
+    }
+    connection.query(`SELECT * FROM forums WHERE id = ${req.params.id}`, function (error, result) {
+        if (error) throw error;
+        if(result.length > 0) {
+            res.render('admin-forums-edit', { config: config, group: req.session.group, user: req.session.user, forum: result[0], error: error });
+        } else {
+            res.redirect('/admin/forums');
+        }
+    });
+});
 //Add forum
 router.post('/forums/new', body('name').not().isEmpty().trim().escape(), body("description").not().isEmpty().trim().escape().isLength({max:256}), body("identifier").not().isEmpty().trim().escape().isLength({max:5}), function(req, res) {
     if(!req.session.loggedIn || req.session.group < 1) {
@@ -40,8 +58,22 @@ router.post('/forums/new', body('name').not().isEmpty().trim().escape(), body("d
         res.redirect('/admin/forums');
     });
 });
-
-
+//Edit forum
+router.post('/forums/:id/edit', body('name').not().isEmpty().trim().escape(), body("description").not().isEmpty().trim().escape().isLength({max:256}), body("identifier").not().isEmpty().trim().escape().isLength({max:5}), function(req, res) {
+    if(!req.session.loggedIn || req.session.group < 1) {
+        return res.redirect('/');
+    }
+    let name = req.body.name;
+    let description = req.body.description;
+    let identifier = req.body.identifier;
+    if(name == "" || description == "" || identifier == "") { //if any fields are empty
+        return res.redirect('/admin/forums/'+req.params.id+'?error='+encodeURIComponent("Please fill out all fields."));
+    }
+    connection.query(`UPDATE forums SET text_identifier = '${identifier}', name = '${name}', description = '${description}' WHERE id = ${req.params.id}`, function (error, result) {
+        if (error) throw error;
+        res.redirect('/admin/forums');
+    });
+});
 
 
 
