82431e71fed7b426debe9fe68d0f28e04cabf6a8
Simon Novak <sen@onee.ch>
2022-10-25 20:49:52 -0400

    User editing and deletion. I am tired.

--- routes/admin.js
+++ routes/admin.js
@@ -14,6 +14,9 @@ router.get('/', function(req, res) {
     }
     res.render('admin', { config: config, group: req.session.group, user: req.session.user });
 });
+
+//Forum management block
+
 //Manage forums
 router.get('/forums', function(req, res) {
     let err = null
@@ -97,6 +100,75 @@ router.get('/forums/:id/delete', function(req, res) {
             if (error) throw error;
             res.redirect('/admin/forums');
         });
+    });
+});
+
+// User management block
+
+//Manage users
+router.get('/users', function(req, res) {
+    let err = null
+    if(req.query.error) {
+        err = req.query.error;
+    }
+    if(!req.session.loggedIn || req.session.group < 1) {
+        return res.redirect('/');
+    }
+    connection.query(`SELECT * FROM accounts`, function (error, result) {
+        if (error) throw error;
+        res.render('admin-users', { config: config, group: req.session.group, user: req.session.user, users: result, error: err });
+    });
+});
+//Edit user
+router.get('/users/:id', function(req, res) {
+    let err = null
+    if(req.query.error) {
+        err = req.query.error;
+    }
+    if(!req.session.loggedIn || req.session.group < 1) {
+        return res.redirect('/');
+    }
+    connection.query(`SELECT * FROM accounts WHERE id = ${req.params.id}`, function (error, result) {
+        if (error) throw error;
+        if(result.length > 0) {
+            res.render('admin-users-edit', { config: config, group: req.session.group, user: req.session.user, user: result[0], error: err });
+        } else {
+            res.redirect('/admin/users');
+        }
+    });
+});
+//Edit user
+router.post('/users/:id/edit', body('username').not().isEmpty().trim().escape(), body("email").optional({checkFalsy: true}).trim().escape().isEmail(), body("group").not().isEmpty().trim().escape().isInt(), function(req, res) {
+    let err = null
+    if(req.query.error) {
+        err = req.query.error;
+    }
+    if(!req.session.loggedIn || req.session.group < 1) {
+        return res.redirect('/');
+    }
+    let username = req.body.username;
+    let email = req.body.email;
+    let group = req.body.group;
+    if(username == "" || group == "") { //if any fields are empty
+        return res.redirect('/admin/users/'+req.params.id+'?error='+encodeURIComponent("Please fill out all fields."));
+    }
+    connection.query(`UPDATE accounts SET username = '${username}', email = '${email}', group = ${group} WHERE id = ${req.params.id}`, function (error, result) {
+        if (error) throw error;
+        res.redirect('/admin/users');
+    });
+});
+//Delete user
+router.get('/users/:id/delete', function(req, res) {
+    let err = null
+    if(req.query.error) {
+        err = req.query.error;
+    }
+    if(!req.session.loggedIn || req.session.group < 1) {
+        return res.redirect('/');
+    }
+    connection.query(`DELETE FROM accounts WHERE id = ${req.params.id}`, function (error, result) {
+        if (error) throw error;
+        res.redirect('/admin/users');
     });
 });
 
