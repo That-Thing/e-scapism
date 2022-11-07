406a5b1577fcb1aa634b1efd24e0bee2b5baee5c
That Thing <sen@onee.ch>
2022-11-07 10:00:24 -0500

    Added replies. Changed some templates.

--- routes/forums.js
+++ routes/forums.js
@@ -70,5 +70,34 @@ router.get('/:id/:post', function(req, res, next) {
         }
     });
 });
+//Reply to post
+router.post('/:id/:post/post', body('content').not().isEmpty().trim().escape(), function(req, res, next) {
+    if(!req.session.loggedIn) {
+        return res.redirect('/login');
+    }
+    let content = req.body.content;
+    if(content == "") {
+        return res.redirect(`/forums/${req.params.id}/${req.params.post}`);
+    }
+    //Check if forum exists
+    connection.query(`SELECT * FROM forums WHERE id = ${req.params.id}`, function (error, rows) {
+        if(rows.length > 0) {
+            //Check if thread exists
+            connection.query(`SELECT * FROM posts WHERE id = ${req.params.post} AND thread IS NULL AND forum=${req.params.id}`, function (error, rows) {
+                if(rows.length > 0) {
+                    connection.query(`INSERT INTO posts VALUES (NULL, ${req.session.user}, '${req.session.username}', ${req.params.id}, ${req.params.post},${Date.now()}, '${content}')`, function (error, result) {
+                        if (error) throw error;
+                        res.redirect(`/forums/${req.params.id}/${req.params.post}#${result.insertId}`); //Redirect user to post in thread.
+                    });
+                } else {
+                    return res.redirect('/forums');
+                }
+            });
+        } else {
+            return res.redirect('/forums');
+        }
+    });
+});
+
 
 module.exports = router;
