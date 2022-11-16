9b0bb4b14d0e1a7ba9b914ddf65e588ad6e12a4d
That Thing <sen@onee.ch>
2022-11-16 15:43:06 -0500

    Added makrdown checking (doesn't fully work but the basics are there) Created separate css file for markdown styling.

--- routes/forums.js
+++ routes/forums.js
@@ -11,6 +11,32 @@ const { body, validationResult } = require('express-validator');
 function checkWords (str) {
     return str.split(" ").length;
 }
+//Check if any markdown is present in a string 
+function checkMarkdown(str) {
+    str = str.replace("&#x2F;", "/");
+    Array.from(str.matchAll(/\*\*\*[^*]+\*\*\*/gm)).forEach((match) => { //Bold Italics
+        str = str.replace(match[0], `<b><i>${match[0].substring(3,match[0].length-3)}</i></b>`);
+    });
+    Array.from(str.matchAll(/\*\*[^*]+\*\*/gm)).forEach((match) => { //Bold
+        str = str.replace(match[0], `<b>${match[0].substring(2,match[0].length-2)}</b>`);
+    });
+    Array.from(str.matchAll(/\*[^*]+\*/gm)).forEach((match) => { //Italics
+        str = str.replace(match[0], `<i>${match[0].substring(1,match[0].length-1)}</i>`);
+    });
+    Array.from(str.matchAll(/\[img\]([^]]+)\[\/img\]/gm)).forEach((match) => { //Image
+        str = str.replace(match[0], `<img src="${match[1].substring(4, match[0].length-4)}">`);
+    });
+    Array.from(str.matchAll(/\[quote=([^]]+)\]([^]]+)\[\/quote\]/gm)).forEach((match) => { //Quote
+        str = str.replace(match[0], `<blockquote><b>${match[1]} said:</b><br>${match[2]}</blockquote>`);
+    });
+    Array.from(str.matchAll(/\[code\]([^]]+)\[\/code\]/gm)).forEach((match) => { //Code
+        str = str.replace(match[0], `<code>${match[1]}</code>`);
+    });
+    Array.from(str.matchAll(/\[spoiler\]([^]]+)\[\/spoiler\]/gm)).forEach((match) => { //Spoiler
+        str = str.replace(match[0], `<span class="spoiler">${match[1]}</span>`);
+    });
+    return str;
+}
 
 //Forum list page
 router.get('/', function(req, res, next) {
@@ -29,6 +55,10 @@ router.get('/:id', function(req, res, next) {
     connection.query(`SELECT * FROM forums WHERE id = ${req.params.id}`, function (error, rows) { //Get forum
         if(rows.length > 0) {
             connection.query(`SELECT * FROM posts WHERE forum=${req.params.id} AND thread IS NULL`, function (error, posts) { //Get posts
+                //For each post, get post content
+                posts.forEach(post => {
+                    post.content = checkMarkdown(post.content);
+                });
                 res.render('forum', { config: config, forum: rows[0], posts: posts, group: req.session.group, loggedIn: req.session.loggedIn, error: err });
             });
         } else {
