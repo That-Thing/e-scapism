18fc01a47c3e7ad36b1cfd1fe1c76bc0d7439e75
That Thing <sen@onee.ch>
2023-04-18 13:27:58 -0400

    Added config updating through admin panel.

--- modules/config.js
+++ modules/config.js
@@ -1,7 +1,7 @@
 const fs = require('fs'); //filesync
 let config = JSON.parse(fs.readFileSync('config/config.json'));
 function reloadConfig() {
-    config = JSON.parse(fs.readFileSync('config.json'));
+    config = JSON.parse(fs.readFileSync('config/config.json'));
     return config;
 }
 module.exports = config;
--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const config = require('../modules/config');
 const connection = require('../modules/connection');
 const { body, validationResult } = require('express-validator');
+const {writeFile} = require("fs");
+
+router.use(function(req, res, next) {
+    if(!req.session.loggedIn || req.session.group < 1) {
+        return res.redirect('/');
+    }
+    next();
+})
+
 
 //Get admin page
 router.get('/', function(req, res) {
-    if(!req.session.loggedIn) { //If user not logged in
-        return res.redirect('/login');
-    }
-    if(req.session.group < 1) { //If user is not admin
-        return res.redirect('/');
-    }
     res.render('admin', { config: config, group: req.session.group, user: req.session.user });
 });
 
@@ -22,9 +25,6 @@ router.get('/forums', function(req, res) {
     let err = null
     if(req.query.error) {
         err = req.query.error;
-    }
-    if(!req.session.loggedIn || req.session.group < 1) {
-        return res.redirect('/');
     }
     connection.query(`SELECT * FROM forums`, function (error, result) {
         if (error) throw error;
@@ -37,9 +37,6 @@ router.get('/forums/:id', function(req, res) {
     if(req.query.error) {
         err = req.query.error;
     }
-    if(!req.session.loggedIn || req.session.group < 1) {
-        return res.redirect('/');
-    }
     connection.query(`SELECT * FROM forums WHERE id = ${req.params.id}`, function (error, result) {
         if (error) throw error;
         if(result.length > 0) {
@@ -51,9 +48,6 @@ router.get('/forums/:id', function(req, res) {
 });
 //Add forum
 router.post('/forums/new', body('name').not().isEmpty().trim().escape(), body("description").not().isEmpty().trim().escape().isLength({max:256}), body("identifier").not().isEmpty().trim().escape().isLength({max:5}), function(req, res) {
-    if(!req.session.loggedIn || req.session.group < 1) {
-        return res.redirect('/');
-    }
     let name = req.body.name;
     let description = req.body.description;
     let identifier = req.body.identifier;
@@ -74,9 +68,6 @@ router.post('/forums/new', body('name').not().isEmpty().trim().escape(), body("d
 });
 //Edit forum
 router.post('/forums/:id/edit', body('name').not().isEmpty().trim().escape(), body("description").not().isEmpty().trim().escape().isLength({max:256}), body("identifier").not().isEmpty().trim().escape().isLength({max:5}), function(req, res) {
-    if(!req.session.loggedIn || req.session.group < 1) {
-        return res.redirect('/');
-    }
     let name = req.body.name;
     let description = req.body.description;
     let identifier = req.body.identifier;
@@ -90,8 +81,9 @@ router.post('/forums/:id/edit', body('name').not().isEmpty().trim().escape(), bo
 });
 //Delete forum
 router.get('/forums/:id/delete', function(req, res) {
-    if(!req.session.loggedIn || req.session.group < 1) {
-        return res.redirect('/');
+    let err = null
+    if(req.query.error) {
+        err = req.query.error;
     }
     connection.query(`DELETE FROM forums WHERE id = ${req.params.id}`, function (error, result) {
         if (error) throw error;
@@ -111,9 +103,6 @@ router.get('/users', function(req, res) {
     if(req.query.error) {
         err = req.query.error;
     }
-    if(!req.session.loggedIn || req.session.group < 1) {
-        return res.redirect('/');
-    }
     connection.query(`SELECT * FROM accounts`, function (error, result) {
         if (error) throw error;
         res.render('admin-users', { config: config, group: req.session.group, user: req.session.user, users: result, error: err });
@@ -124,9 +113,6 @@ router.get('/users/:id', function(req, res) {
     let err = null
     if(req.query.error) {
         err = req.query.error;
-    }
-    if(!req.session.loggedIn || req.session.group < 1) {
-        return res.redirect('/');
     }
     connection.query(`SELECT * FROM accounts WHERE id = ${req.params.id}`, function (error, result) {
         if (error) throw error;
@@ -139,13 +125,6 @@ router.get('/users/:id', function(req, res) {
 });
 //Edit user
 router.post('/users/:id/edit', body('username').not().isEmpty().trim().escape(), body("email").optional({checkFalsy: true}).trim().escape().isEmail(), body("group").not().isEmpty().trim().escape().isInt(), function(req, res) {
-    let err = null
-    if(req.query.error) {
-        err = req.query.error;
-    }
-    if(!req.session.loggedIn || req.session.group < 1) {
-        return res.redirect('/');
-    }
     let username = req.body.username;
     let email = req.body.email;
     if (email == "") {
@@ -162,13 +141,6 @@ router.post('/users/:id/edit', body('username').not().isEmpty().trim().escape(),
 });
 //Delete user
 router.get('/users/:id/delete', function(req, res) {
-    let err = null
-    if(req.query.error) {
-        err = req.query.error;
-    }
-    if(!req.session.loggedIn || req.session.group < 1) {
-        return res.redirect('/');
-    }
     connection.query(`DELETE FROM accounts WHERE id = ${req.params.id}`, function (error, result) {
         if (error) throw error;
         res.redirect('/admin/users');
@@ -180,11 +152,59 @@ router.get('/settings', function(req, res) {
     if(req.query.error) {
         err = req.query.error;
     }
-    if(!req.session.loggedIn || req.session.group < 1) {
-        return res.redirect('/');
-    }
     res.render('admin-settings', { config: config, group: req.session.group, user: req.session.user, error: err });
 });
+router.post('/settings/update',
+    body("name").not().isEmpty().trim().escape(),
+    body("description").not().isEmpty().trim().escape(),
+    body("logo").not().isEmpty().trim().escape(),
+    body("favicon").not().isEmpty().trim().escape(),
+    //Registration
+    body("registrations").not().isEmpty().trim().escape().isBoolean(),
+    body("require-email").not().isEmpty().escape().isBoolean(),
+    body("email-confirmation").not().isEmpty().escape().isBoolean(),
+    //Threads
+    body("thread-wordcount").not().isEmpty().escape().isBoolean(),
+    body("thread-minimum-words").not().isEmpty().isNumeric(),
+    body("thread-maximum-words").not().isEmpty().isNumeric(),
+    body("thread-max-chars").not().isEmpty().isNumeric(),
+    //Posts
+    body("post-wordcount").not().isEmpty().escape().isBoolean(),
+    body("post-minimum-words").not().isEmpty().isNumeric(),
+    body("post-maximum-words").not().isEmpty().isNumeric(),
+    body("post-max-chars").not().isEmpty().isNumeric(),
+    function(req, res) {
+        //Write information to config.json file
+        config.branding.name = req.body.name;
+        config.branding.description = req.body.description;
+        //config.branding.logo = req.body.logo;
+        //config.branding.favicon = req.body.favicon;
+        //Registration
+        req.body.registration == "true" ? config.accounts.registration.enabled = true : config.accounts.registration.enabled = false;
+        req.body['require-email'] == "true" ? config.accounts.registration.requireEmail = true : config.accounts.registration.requireEmail = false;
+        req.body['email-confirmation'] == "true" ? config.accounts.registration.requireEmailConfirmation = true : config.accounts.registration.requireEmailConfirmation = false;
+        //Threads
+        req.body['thread-wordcount'] == "true" ? config.posts.wordCount.threads.enabled = true : config.posts.wordCount.threads.enabled = false;
+        config.posts.wordCount.threads.minimum = parseInt(req.body['thread-minimum-words']);
+        config.posts.wordCount.threads.maximum = parseInt(req.body['thread-maximum-words']);
+        config.posts.wordCount.threads.maxChars = parseInt(req.body['thread-max-chars']);
+        //Posts
+        req.body['post-wordcount'] == "true" ? config.posts.wordCount.replies.enabled = true : config.posts.wordCount.replies.enabled = false;
+        config.posts.wordCount.replies.minimum = parseInt(req.body['post-minimum-words']);
+        config.posts.wordCount.replies.maximum = parseInt(req.body['post-maximum-words']);
+        config.posts.wordCount.replies.maxChars = parseInt(req.body['post-max-chars']);
+        //Write to file
+        writeFile('./config/config.json', JSON.stringify(config, null, 4), function(err) {
+            if(err) {
+                return console.log(err);
+            }
+            res.redirect('/admin/settings?success='+encodeURIComponent("Settings updated successfully."));
+        });
+
+
+
+});
+
 
 
 
